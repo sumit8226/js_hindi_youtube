d40aa8b30b69e6689ea81f3ba088dee90700ac04
sumit8226 <86927075+sumit8226@users.noreply.github.com>
2025-08-20 14:06:56 +0000

    data type

---
+++ basics/dataType.js
@@ -0,0 +1,30 @@
+const varn ="four"
+var age =18
+var name ="sumit"
+
+
+
+
+
+
+
+
+//number => 2 to power 53
+// Bigint
+// string => ""
+// boolean =>true/false
+//null => standalone value and type  object
+//undefined =>
+// symbol => unique
+
+console.log(typeof undefined)
+
+console.log(typeof null)
+console.log(typeof varn)
+console.log(typeof age)
+console.log(typeof "sumit")
+console.log(typeof null)
+
+
+
+
